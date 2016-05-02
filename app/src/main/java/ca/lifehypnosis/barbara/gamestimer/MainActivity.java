671a73ba459e6f30d10f0f046c2af08bcfb6c99b
Barbara Samson <barbarasamson@gmail.com>
2016-05-02 15:43:23 -0400

    Got timer bar to show up at top, but it is not moving down properly yet.

--- app/src/main/java/ca/lifehypnosis/barbara/gamestimer/MainActivity.java
+++ app/src/main/java/ca/lifehypnosis/barbara/gamestimer/MainActivity.java
@@ -2,8 +2,10 @@ package ca.lifehypnosis.barbara.gamestimer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-
+        startTimer();
     }
 
     @Override
@@ -35,4 +37,22 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void startTimer() { // 3 hours, increment every 6 seconds
+        final View timerBar = findViewById(R.id.timerBar);
+        timerBar.setVisibility(View.VISIBLE);
+        new CountDownTimer(10800000, 1000) {
+            int yPad = timerBar.getPaddingTop();
+            public void onTick(long millisUntilFinished) {
+                timerBar.setPadding(0, yPad + 10, 0, 0);
+                yPad = timerBar.getPaddingTop();
+            }
+
+            @Override
+            public void onFinish() {
+                timerBar.setVisibility(View.GONE);
+            }
+        }.start();
+    }
+
 }
